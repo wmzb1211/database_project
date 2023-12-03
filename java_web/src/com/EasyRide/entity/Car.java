3f856e19843d6743f57c58a2d9d59763b7a7e402
任柒曜 <128783876+Rslaem@users.noreply.github.com>
2023-12-03 23:02:46 +0800

    car improvement
    1

--- java_web/src/com/EasyRide/entity/Car.java
+++ java_web/src/com/EasyRide/entity/Car.java
@@ -25,4 +25,69 @@ public class Car {
     private double dailyRentalFee;
 
     // Getters and setters
+
+    public Car(int carId, int modelId, String plateNumber, String color, int year, String status, double dailyRentalFee) {
+        this.carId = carId;
+        this.modelId = modelId;
+        this.plateNumber = plateNumber;
+        this.color = color;
+        this.year = year;
+        this.status = status;
+        this.dailyRentalFee = dailyRentalFee;
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+
+
+    public int getModelId() {
+        return modelId;
+    }
+
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getDailyRentalFee() {
+        return dailyRentalFee;
+    }
+
+    public void setDailyRentalFee(double dailyRentalFee) {
+        this.dailyRentalFee = dailyRentalFee;
+    }
+    public List<Car> getAllCarsByStatus(String status){
+        CarDAO carDAO = new CarDAO();
+        return carDAO.getAllCarsByStatus(status);
+    }
 }
+

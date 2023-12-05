53d87d12891be30a85e61e2065bbf16501338ebf
任柒曜 <128783876+Rslaem@users.noreply.github.com>
2023-12-05 12:51:15 +0800

    11

--- java_web/src/com/EasyRide/entity/Administrator.java
+++ java_web/src/com/EasyRide/entity/Administrator.java
@@ -11,5 +11,70 @@ public class Administrator {
     private String role;
     private Date creationDate;
 
-    // Getters and setters
+    public Administrator(int adminId, String name, String username, String password, String contactInfo, String role, Date creationDate) {
+        this.adminId = adminId;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.contactInfo = contactInfo;
+        this.role = role;
+        this.creationDate = creationDate;
+    }
+// Getters and setters
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
 }
